8f6971c71b57ed8c32bb35b1f7535d456a3768dc
Emanuel Laporta <emanuel@DESKTOP-HHKILBO.localdomain>
2020-07-02 17:19:05 -0300

    kellycolorpicker moved inside script.js

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -4,7 +4,7 @@ import { WEBGL } from './three.js/examples/jsm/loaders/WEBGL.js';
 import { GLTFLoader } from './three.js/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from './three.js/examples/jsm/controls/OrbitControls.js';
 import { COLORS } from './colors.js';
-
+import { KellyColorPicker } from './html5kellycolorpicker/html5kellycolorpicker.min.js';
 // import { TDSLoader } from './three.js/examples/jsm/loaders/TDSLoader.js';
 // import { MTLLoader } from './three.js/examples/jsm/loaders/MTLLoader.js';
 // import { OBJLoader } from './three.js/examples/jsm/loaders/OBJLoader.js';
@@ -16,7 +16,6 @@ const DRAG_NOTICE = document.getElementById('js-drag-notice');
 
 let theModel;
 let activeOption = 'all';
-const colors = COLORS;
 
 const BACKGROUND_COLOR = 0xf1f1f1;
 const INITIAL_MTL = new THREE.MeshPhongMaterial({ color: 0xF2DABA, shininess: 10 });
@@ -29,12 +28,15 @@ scene.background = new THREE.Color(BACKGROUND_COLOR);
 const container = document.getElementById('model-container');
 const renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
 renderer.setSize(container.offsetHeight, container.offsetHeight);
-renderer.shadowMap.enabled = false;
+renderer.shadowMap.enabled = true;
+// renderer.shadowMapType = THREE.PCFSoftShadowMap;
+// renderer.physicallyCorrectLights = true;
 container.appendChild(renderer.domElement);
 
 // Add a camera
 let camera = new THREE.PerspectiveCamera(35, container.offsetHeight / container.offsetHeight, 0.1, 1000);
-camera.position.set(0, 0, 2);
+// camera.position.set(0, 0, 2);
+camera.position.set(-0.2963585789463235, 1.2246467991473552e-16, 4.230488252069703);
 camera.rotation.set(0, 0, 0);
 resize();
 
@@ -74,18 +76,34 @@ controls.dampingFactor = 0.25;
 // controls.minDistance = 30;
 // controls.maxDistance = 190;
 
-// Add hemisphere light to scene   
-let hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
-hemisphereLight.position.set(0, 50, 0);
-scene.add(hemisphereLight);
+// Add hemisphere light to scene
+// let hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x080820, 0.61);
+// hemisphereLight.position.set(0, 50, 0);
+// scene.add(hemisphereLight);
+
+// Add ambient light
+let ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(ambientLight);
 
 // Add directional light to scene
 let directionalLight = new THREE.DirectionalLight(0xfffedb, 0.50);
-// directionalLight.position.set(-8, 12, 8);
-directionalLight.position.set(30, 5, 0);
+directionalLight.position.set(-9, 1.5, 2);
 directionalLight.castShadow = true;
-directionalLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+directionalLight.shadow.mapSize = new THREE.Vector2(2048, 2048);
 scene.add(directionalLight);
+
+// Directional light helper
+let directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+scene.add(directionalLightHelper);
+
+// Add point light to scene
+let pointLight = new THREE.PointLight(0xfffedb, 1, 100);
+pointLight.position.set(-0.5, 1.2, 0);
+scene.add(pointLight);
+
+// Point light helper
+let pointLightHelper = new THREE.PointLightHelper(pointLight, 1);
+scene.add(pointLightHelper);
 
 function setObjectNameId(name = null){
     let nameId = null;
@@ -119,9 +137,11 @@ let loadergManager = new THREE.LoadingManager(function(){
                 // Set shadows
                 o.castShadow = true;
                 o.receiveShadow = true;
+                // o.shininess = 0;
 
                 if(o.name == 'Ventana_1_(Agua1)' || o.name == 'Ventana_2_(Agua_1)'){
                     o.material.opacity = 0.08;
+                    o.visible = false;
                 }
 
                 // Find walls
@@ -189,6 +209,15 @@ loader.load('living.gltf', function(object){
     theModel = object.scene.children[0];
 });
 
+function debugInfo(){
+    if(theModel){
+        console.log('Model Position: ', theModel.position);
+        console.log('Model Rotation: ', theModel.rotation);
+    }
+    console.log('Camera Position: ', camera.position);
+    console.log('Camera Rotation: ', camera.rotation);
+}
+
 // Key controls movement
 document.onkeydown = function(e){
     switch(e.keyCode){
@@ -203,6 +232,11 @@ document.onkeydown = function(e){
         break;
         case 40: // down
             camera.rotation.z += 0.1;
+        break;
+
+        // debug info
+        case 73: // s
+            debugInfo();
         break;
 
         // the model position for up/down
@@ -242,14 +276,11 @@ function start(){
     document.getElementById('sidebar-open-btn').click();
 }
 
-function aca(){
-    console.log('aca');
-}
-
 // Function - Build Colors
 function buildColors(colors) {
     let colorsLength = colors.length -1;
     let counter = 0;
+    TRAY.innerHTML = '';
     let tr = document.createElement('tr');
     tr.classList.add('tray__swatch__group');
 
@@ -265,9 +296,9 @@ function buildColors(colors) {
 
         let swatch = document.createElement('td');
         swatch.classList.add('tray__swatch');
-        swatch.style.background = '#' + color.color;
+        swatch.style.background = 'rgb(' + color.r + ', ' + color.g + ', ' + color.b + ')';
         swatch.setAttribute('data-key', i);
-        swatch.setAttribute('title', '0x' + color.color);
+        // swatch.setAttribute('title', 'rgb(' + color.r + ', ' + color.g + ', ' + color.b + ')');
         tr.append(swatch);
 
         if(i == colorsLength){
@@ -276,7 +307,22 @@ function buildColors(colors) {
     }
 }
 
-buildColors(colors);
+// buildColors(COLORS);
+
+// Init color picker
+const kellyColorPicker = new KellyColorPicker({
+    place : 'color-picker',
+    size : 150,
+    inputFormat: 'rgba',
+    userEvents: {
+        change: function(e){
+            console.log(e.getCurColorRgb());
+
+            let newColors = COLORS.slice(0, 16);
+            buildColors(newColors);
+        }
+    }
+});
 
 // Select Option
 const options = document.querySelectorAll('.option');
@@ -304,7 +350,7 @@ for(const swatch of swatches){
 }
 
 function selectSwatch(e) {
-    let color = colors[parseInt(e.target.dataset.key)];
+    let color = COLORS[parseInt(e.target.dataset.key)];
     let new_mtl;
     let option = e.target;
 
